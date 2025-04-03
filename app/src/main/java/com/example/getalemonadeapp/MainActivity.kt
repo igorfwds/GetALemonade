671fa678432cdad32b22bc4ca97d81a19f0f525e
Igor Wanderley <ifws@cesar.org.br>
2025-04-03 17:14:11 -0300

    Added a onLongPress behavior in LEMON view

--- app/src/main/java/com/example/getalemonadeapp/MainActivity.kt
+++ app/src/main/java/com/example/getalemonadeapp/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -30,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -100,6 +104,7 @@ fun GetLemonadeApp(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CoreView(
     clickCount: Int,
@@ -110,7 +115,8 @@ fun CoreView(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -128,10 +134,18 @@ fun CoreView(
             }
         }
 
-        Surface(
+        Image(
+            painter = painterResource(imagePainter),
+            contentDescription = stringResource(imageDescription),
             modifier = modifier
+                .clip(RoundedCornerShape(4.dp))
                 .padding(bottom = 16.dp)
-                .clickable {
+                .border(
+                    shape = RoundedCornerShape(4.dp),
+                    border = BorderStroke(2.dp, colorResource(R.color.button_stroke)),
+                )
+                .background(colorResource(R.color.button_background))
+                .combinedClickable(onClick = {
                     clicks++
 
                     if (imageDescription == R.string.lemon) {
@@ -155,15 +169,13 @@ fun CoreView(
                         clicks = 0
                     }
 
-                },
-            shape = RoundedCornerShape(18.dp)
-        ) {
-            Image(
-                painter = painterResource(imagePainter),
-                contentDescription = stringResource(imageDescription),
-                modifier = modifier.background(colorResource(R.color.button_background))
-            )
-        }
+                }, onLongClick = {
+                    if (imageDescription == R.string.lemon) {
+                        onImageClick()
+                        clicks = 0
+                    }
+                })
+        )
         Text(
             text = if (imageDescription == R.string.lemon) {
                 stringResource(textContent) + " (${clicks}/${randomClickGoal})"
@@ -173,6 +185,7 @@ fun CoreView(
         )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
